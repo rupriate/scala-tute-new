f7f1e6c6b1cf896347081e8d49eae2044a62990e
jayathmaa <rusirasathnindu50@gmail.com>
2023-09-19 09:53:23 +0530

    .

--- 10/1.scala
+++ 10/1.scala
@@ -1,16 +1,18 @@
-object first {
+object q1 {
+
+  def calculateAverage(temperatures: List[Double]): Double = {
+    val F_Temp = temperatures.map(_ * 9/5 + 32)
+    val total = F_Temp.reduce(_ + _)
+    val avg = total / temperatures.length
+    avg
+  }
+
+
   def main(args: Array[String]): Unit = {
-    val C_Temp = List(0.0, 10.0, 20.0, 30.0, 40.0)
-    val average_F = cal_Avg(C_Temp)
+    val C_Temp = List(0.0, 10.0, 20.0, 30.0)
+    val average_F = calculateAverage(C_Temp)
     println(s"Average Fahrenheit Temperature: $average_F")
   }
 
-  def cal_Avg(tempList: List[Double]): Double = {
-
-    val F_Temp = tempList.map(celsius => (celsius * 9 / 5) + 32)
-    println(F_Temp)
-    val result = F_Temp.reduce((_ + _))
-    val avg = result / tempList.length.toDouble
-    avg
-  }
+  
 }
